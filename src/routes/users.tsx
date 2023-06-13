e7b4e68a491d9438c47219ba5e00bb42fbef6d52
danielhollanderXyte <>
2023-06-13 10:03:19 +0300

    changed text in users

--- src/routes/users.tsx
+++ src/routes/users.tsx
@@ -1,7 +1,6 @@
 import UserItem from "../components/User/User";
-import { FC, useState } from "react";
+import { FC } from "react";
 import User from "../Models/User";
-import { useParams } from "react-router-dom";
 
 interface UsersList {
   usersList: User[];
